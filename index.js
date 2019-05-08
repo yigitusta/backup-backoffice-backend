661fa01e923df9ab96ee9f7ccfc3762a73e6f460
Fatih Yavuz <fatihjy@gmail.com>
2019-05-08 05:40:37 +0300

    heroku fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express")
 const cors = require("cors");
 const db = require("./db");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
